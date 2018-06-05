d2d4638f5ce53bc6646a7ccd83800fa42916c849
mfrancis95 <mikefrancis95@gmail.com>
2018-06-05 07:05:15 -0400

    Add /actor{actor-id} route

--- calvin/runtime/north/control_apis/coap_api.py
+++ calvin/runtime/north/control_apis/coap_api.py
@@ -1,10 +1,41 @@
 from txthings import resource
 from txthings.coap import Coap, COAP_PORT, CONTENT, Message
 from json import dumps
+from calvin.utilities.calvin_callback import CalvinCB
 from calvin.runtime.north.calvinsys import get_calvinsys
 from calvin.runtime.north.calvinlib import get_calvinlib
-from twisted.internet.defer import succeed
-from twisted.internet.reactor import listenUDP
+from twisted.internet.defer import Deferred, succeed
+from twisted.internet.reactor import callLater, listenUDP
+
+class _ActorResource(resource.CoAPResource):
+
+    def __init__(self, node):
+        resource.CoAPResource.__init__(self)
+        self.node = node
+
+    def getChild(self, path, request):
+        return _ActorIdResource(self.node, path)
+
+class _ActorIdResource(resource.CoAPResource):
+
+    def __init__(self, node, id):
+        resource.CoAPResource.__init__(self)
+        self.node = node
+        self.id = id
+
+    def get_actor(self, deferred):
+
+        def callback(key, value):
+            message = Message(code = CONTENT, payload = dumps(value))
+            deferred.callback(message)
+
+        id = self.id[:8] + '-' + self.id[8:12] + '-' + self.id[12:16] + '-' + self.id[16:20] + '-' + self.id[20:]
+        self.node.storage.get_actor(id, CalvinCB(callback))
+
+    def render_GET(self, request):
+        deferred = Deferred()
+        callLater(0, self.get_actor, deferred)
+        return deferred
 
 class _ActorsResource(resource.CoAPResource):
 
@@ -41,6 +72,7 @@ class CoAPServer:
 
     def __init__(self, node):
         self.root = resource.CoAPResource()
+        self.root.putChild('actor', _ActorResource(node))
         self.root.putChild('actors', _ActorsResource(node))
         self.root.putChild('capabilities', _CapabilitiesResource())
         self.root.putChild('id', _IdResource(node))
